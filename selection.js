66269833d7252c8c428b70c6e4849c6dd7fa3ced
cwverica <cwverica@gmail.com>
2021-10-27 11:40:54 -0700

    added selection

--- selection.js
+++ selection.js
@@ -1,3 +1,16 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minimum = [arr[i], i];
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < minimum[0]) {
+                minimum = [arr[j], j];
+            }
+        }
+        let temp = arr[i];
+        arr[i] = minimum[0];
+        arr[minimum[1]] = temp;
+    }
+    return arr;
+}
 
 module.exports = selectionSort;
